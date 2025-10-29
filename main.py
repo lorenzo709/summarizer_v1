ce2352c66319e9531d47f527d344396b9db64852
LORENZO DIROSA s329169 <s329169@studenti.polito.it>
2025-10-29 16:34:48 +0100

    added reviewer for pro and con

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from crewai.project import CrewBase, agent, crew, task
 
 from crewai_tools import (
     SerperDevTool,
-    # ScrapeWebsiteTool,
+    ScrapeWebsiteTool,
     # DirectoryReadTool,
     # FileWriterTool,
     # FileReadTool,
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = LLM(model="ollama/falcon:7b", base_url="http://localhost:11434")
+llm = LLM(model="ollama/deepseek-r1:8b", base_url="http://localhost:11434")
 
 
 @CrewBase
@@ -26,15 +26,14 @@ class BlogCrew:
     @agent
     def researcher(self) -> Agent:
         return Agent(
-            # config=self.agents_config["research_agent"],  # type: ignore[index]
+            # tools=[SerperDevTool(),ScrapeWebsiteTool()],
             tools=[SerperDevTool()],
             llm="gemini/gemini-2.0-flash",
             role="Scientific Papers Researcher Specialist",
             goal="Accurately search the internet to find pertinent scientific paper",
             backstory=(
                 "You are a meticulous researcher, and your primary function is "
-                "to use your search tool to gather data. You MUST follow the "
-                "function-call format exactly when using your tools."  # A hint!
+                "to use your search tool to gather data."
             ),
             verbose=True,
         )
@@ -42,26 +41,46 @@ class BlogCrew:
     @agent
     def writer(self) -> Agent:
         return Agent(
-            # config=self.agents_config["paper_writer_agent"],  # type: ignore[index]
-            # llm=llm,
-            llm="gemini/gemini-2.0-flash",
+            llm=llm,
+            # llm="gemini/gemini-2.0-flash",
+            tool = [ScrapeWebsiteTool()],
             role="Expert Summarization Writer",
             goal="Write a short summarization of the scientific papers from the previous task",
             backstory="You are an experience writer in summarizing a text",
             verbose=True,
         )
 
+    @agent 
+    def reviewer(self) -> Agent:
+        return Agent(
+            llm=llm,
+            # llm="gemini/gemini-2.0-flash",
+            role="Academic Reviewer Specialist",
+            goal="""To meticulously analyze a given academic paper, identifying
+            its core contributions, strengths (pros), and areas for improvement
+            or shortcomings (limitations), and structure these findings into two
+            distinct, well-reasoned lists""",
+            backstory="""You spent a decade as a peer reviewer for top-tier
+            academic journals in your field. You are known for your objective,
+            balanced, and sharp analytical mind. You believe that all research,
+            no matter how groundbreaking, has inherent boundaries, and your sole
+            purpose is to provide an honest, unbiased, and constructive
+            critique. Your reviews are concise, insightful, and strictly focus
+            on the scientific merit and applicability of the work""",
+            verbose=True,
+        )
     @agent
     def aggregator(self) -> Agent:
         return Agent(
             # config=self.agents_config["paper_writer_agent"],  # type: ignore[index]
-            # llm=llm,
-            llm="gemini/gemini-2.0-flash",
+            llm=llm,
+            # llm="gemini/gemini-2.0-flash",
             role="Expert Summarization WriterAcademic Synthesizer and Narrative Editor",
             goal="""To generate a single, continuous, and easy-to-read document
             (400–600 words) that seamlessly integrates the content of 5–6
             individual paper summaries, ensuring a logical progression of ideas
-            from start to finish.""",
+            from start to finish.
+            """,
             backstory="""you are the Lead Synthesis Editor for a top-tier
             science communication firm. Your sole job is to take fragmented,
             specialized research summaries and fuse them into a single,
@@ -85,6 +104,12 @@ class BlogCrew:
         )
 
     @task
+    def reviewer_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["reviewer_task"],  # type: ignore[index]
+            agent=self.reviewer(),
+        )
+    @task
     def aggregate_task(self) -> Task:
         return Task(
             config=self.tasks_config["aggregate_task"],  # type: ignore[index]
@@ -94,8 +119,8 @@ class BlogCrew:
     @crew
     def crew(self) -> Crew:
         return Crew(
-            agents=[self.researcher(), self.writer(), self.aggregator()],
-            tasks=[self.research_task(), self.summarize_task(), self.aggregate_task()],
+            agents=[self.researcher(), self.writer(), self.aggregator(),self.reviewer()],
+            tasks=[self.research_task(), self.summarize_task(), self.aggregate_task(),self.reviewer_task()],
         )
 
 
