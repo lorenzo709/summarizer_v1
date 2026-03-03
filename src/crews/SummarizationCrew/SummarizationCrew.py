b02e8fbb1ff74381da6251de252b21f922684467
LORENZO DIROSA s329169 <s329169@studenti.polito.it>
2026-03-03 17:24:32 +0100

    changed reasoning to none

--- src/crews/SummarizationCrew/SummarizationCrew.py
+++ src/crews/SummarizationCrew/SummarizationCrew.py
@@ -17,7 +17,7 @@ load_dotenv()
 # llm = LLM(model="ollama/qwen2.5:72b", base_url="http://localhost:11434")
 # llm = LLM(model="ollama/llama3.1:70b", base_url="http://localhost:11434")
 # llm = LLM(model="ollama/llama4:scout", base_url="http://localhost:11434")
-llm = LLM(model="ollama/qwen3.5:122b", base_url="http://localhost:11434", reasoning_effort='low')
+llm = LLM(model="ollama/qwen3.5:122b", base_url="http://localhost:11434", reasoning_effort='none')
 @CrewBase
 class SummarizationCrew:
     """research/writing crew"""
