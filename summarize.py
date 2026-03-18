262c26a7e9c417cc7b787dfc6d17108f726de749
LORENZO DIROSA s329169 <s329169@studenti.polito.it>
2026-03-18 10:54:33 +0100

    changed model

--- summarize.py
+++ summarize.py
@@ -16,7 +16,7 @@ from src.crews.CorrectionCrew.CorrectionCrew import CorrectionCrew
 from src.crews.AggregatorCrew.AggregatorCrew import AggregateCrew
 
 TOPIC = "Liquid Neural Networks for Continuous-time Signal Processing."
-MODEL = "llama4:scout"
+MODEL = "gpt-oss:120b"
 
 def setup(rp: ResultPipeLine):
     parsed_papers = []
