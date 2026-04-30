54c280c3cb40d00f099115c1f4e5d6a355ebd5d2
LORENZO DIROSA s329169 <s329169@studenti.polito.it>
2026-04-30 11:49:08 +0200

    changing topic

--- moritoring.sh
+++ moritoring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TOPIC="Retrieval-Augmented_Generation_for_Legacy_Code_Refactoring"
+TOPIC="Zero-Shot_Robot_Manipulation_via_CLIP-based_Spatial_Reasoning"
 MODEL="glm-47-flash_bf16"
 # 1. Setup unique log folder
 # TIMESTAMP=$(date +%Y%m%d_%H%M%S)
