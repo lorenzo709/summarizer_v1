d1f045133ab4a9bb87663ef50e1f98ed15e7da1c
LORENZO DIROSA s329169 <s329169@studenti.polito.it>
2026-06-18 15:37:33 +0200

    changed model and topic

--- moritoring.sh
+++ moritoring.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TOPIC="Liquid_Neural_Networks_for_Continuous-time_Signal_Processing"
-MODEL="gpt-oss:120b"
+TOPIC="Vision_Transformers"
+MODEL="deepseek-r1_70b"
 # 1. Setup unique log folder
 # TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
