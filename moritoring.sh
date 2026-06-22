7687811be4ff19b9119266b9472bec55726553da
LORENZO DIROSA s329169 <s329169@studenti.polito.it>
2026-06-22 18:48:42 +0200

    changed topic

--- moritoring.sh
+++ moritoring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TOPIC="catalytic_water_splitting_on_platinum"
+TOPIC="Zero-Shot_Robot_Manipulation_via_CLIP-based_Spatial_Reasoning"
 MODEL="glm-47-flash_bf16"
 # 1. Setup unique log folder
 # TIMESTAMP=$(date +%Y%m%d_%H%M%S)
