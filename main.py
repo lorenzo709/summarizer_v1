cabaedae5fdfda955d71fa8352f1cf9a95e5cb63
LORENZO DIROSA s329169 <s329169@studenti.polito.it>
2026-06-22 11:10:14 +0200

    changed topic

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ from codecarbon import EmissionsTracker
 
 load_dotenv()
 
-TOPIC = "Vision_Transformers"
+TOPIC = "catalytic_water_splitting_on_platinum"
 MODEL = "deepseek-r1_70b"
 
 OUTPUT_DIR = Path("./output_results_judge_on_final_sum")
@@ -70,8 +70,8 @@ class ResearcherFlow(Flow[ResearcherState]):
             return
         self.state.model = MODEL
         print("Starting to look for interesting papers on topic")
-        self.state.topic = "Vision Transformers (ViT)"
-        # self.state.topic = "catalytic water splitting on platinum"
+        # self.state.topic = "Vision Transformers (ViT)"
+        self.state.topic = "catalytic water splitting on platinum"
         # self.state.topic = "Zero-Shot Robot Manipulation via CLIP-based Spatial Reasoning."
         # self.state.topic = "Retrieval-Augmented Generation for Legacy Code Refactoring."
         # self.state.topic = "Liquid Neural Networks for Continuous-time Signal Processing."
@@ -92,7 +92,7 @@ class ResearcherFlow(Flow[ResearcherState]):
         # topic = "catalytic water splitting on platinum"
         # papers_info = search_and_save_pdf(topic,"./knowledge" )
         # self.state.papers_infos = papers_info
-        folder_path = Path("./knowledge_vision_transformers")
+        folder_path = Path("./knowledge_platinum_water_splitting")
 
         for pdf_file in folder_path.glob("*.pdf"):
             pdf_name = pdf_file.name
